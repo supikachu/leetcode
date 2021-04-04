49befa8f17fb49a68d9c11764015d1750467e05d
Pikachu <yiye-alien@foxmail.com>
2021-04-04 11:38:28 +0800

    add the solution of '0781 rabbits in forest' problem.

--- src/solution.rs
+++ src/solution.rs
@@ -2,3 +2,4 @@ mod s1143_longest_common_subsequence;
 mod s1_two_sum;
 mod s2_add_two_numbers;
 mod s424_longest_repeating_character_replacement;
+mod s781_rabbits_in_forest;
---
+++ src/solution/s781_rabbits_in_forest.rs
@@ -0,0 +1,36 @@
+#[allow(dead_code)]
+struct Solution {}
+
+// [2,2,2,2]
+#[allow(dead_code)]
+#[allow(unused_variables)]
+use std::collections::HashMap;
+impl Solution {
+    pub fn num_rabbits(answers: Vec<i32>) -> i32 {
+        let mut nums = 0;
+        let mut map = HashMap::new();
+        for ans in &answers {
+            let count = map.entry(ans).or_insert(0);
+            *count += 1;
+        }
+        for (kind, num) in map {
+            let k = kind + 1;
+            if kind + 1 < num {
+                nums = nums + (((num as f32) / (k as f32)).ceil() as i32) * k;
+            } else {
+                nums = nums + k;
+            }
+        }
+        nums
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn sol_s781() {
+        assert_eq!(Solution::num_rabbits(vec![1, 1, 2]), 5);
+        assert_eq!(Solution::num_rabbits(vec![10, 10, 10]), 11);
+    }
+}
